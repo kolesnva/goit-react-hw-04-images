4bb27b266bda8823d978ecdb30e4391148b7ad5a
kolesnva <kolesnychenko_v@icloud.com>
2022-09-03 16:25:03 +0300

    modal refactored to hooks

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,40 +1,32 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, ModalWindow } from './ModalStyled';
 
 const modalRoot = document.querySelector('#modal-root');
-export class Modal extends Component {
-  static propTypes = {
-    imageURL: PropTypes.string.isRequired,
-    tags: PropTypes.string.isRequired,
-    closeFunction: PropTypes.func.isRequired,
-  };
+export function Modal({ closeFunction, imageURL, tags }) {
+  useEffect(() => {
+    const onEscPress = event => {
+      if (event.code === 'Escape') closeFunction();
+    };
+    window.addEventListener('keydown', onEscPress);
+    return () => {
+      window.removeEventListener('keydown', onEscPress);
+    };
+  }, [closeFunction]);
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.onEscPress);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onEscPress);
-  }
-
-  onEscPress = event => {
-    if (event.code === 'Escape') {
-      this.props.closeFunction();
-    }
-  };
-
-  render() {
-    const { closeFunction, imageURL, tags } = this.props;
-
-    return createPortal(
-      <Overlay className="overlay" onClick={closeFunction}>
-        <ModalWindow className="modal">
-          <img src={imageURL} alt={tags} />
-        </ModalWindow>
-      </Overlay>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <Overlay className="overlay" onClick={closeFunction}>
+      <ModalWindow className="modal">
+        <img src={imageURL} alt={tags} />
+      </ModalWindow>
+    </Overlay>,
+    modalRoot
+  );
 }
+
+Modal.propTypes = {
+  imageURL: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  closeFunction: PropTypes.func.isRequired,
+};
